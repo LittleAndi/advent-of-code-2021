df067fb987d80f9ef39e673b963d828a0abbc782
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-02 00:53:56 +0100

    Day01 (#1)
    * Quick solution
    
    * Simplify both parts

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt")
+    .Where(l => !string.IsNullOrWhiteSpace(l))
+    .Select(l => int.Parse(l))
+    .ToArray<int>();
+
+int increaseCount = 0;
+for (int i = 1; i < lines.Length; i++)
+{
+    if (lines[i - 1] < lines[i]) increaseCount++;
+}
+
+System.Console.WriteLine($"Part 1: {increaseCount}");
+
+increaseCount = 0;
+for (int i = 3; i < lines.Length; i++)
+{
+    // a + b + c < b + c + d => a < d
+    if (lines[i - 3] < lines[i]) increaseCount++;
+}
+
+System.Console.WriteLine($"Part 2: {increaseCount}");
