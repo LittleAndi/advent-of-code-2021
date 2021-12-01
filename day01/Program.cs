cff6b293c4e43580b680ca18679b20b24d8c2791
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-01 06:23:02 +0100

    Quick solution

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt")
+    .Where(l => !string.IsNullOrWhiteSpace(l))
+    .Select(l => int.Parse(l))
+    .ToArray<int>();
+
+int increase = 0;
+int previous = 10000;
+foreach (var item in lines)
+{
+    if (item > previous) increase++;
+    previous = item;
+}
+
+System.Console.WriteLine(increase);
+
+increase = 0;
+previous = 10000;
+for (int i = 2; i < lines.Length; i++)
+{
+    var sum = lines[i - 2] + lines[i - 1] + lines[i];
+    if (sum > previous) increase++;
+    previous = sum;
+}
+
+System.Console.WriteLine(increase);
