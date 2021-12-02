4280128703e7fdfcef654d65f2f4179522fe6cfb
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-02 06:46:49 +0100

    Solution for day 2

--- tests/day02tests/UnitTest1.cs
+++ tests/day02tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Shouldly;
 using Xunit;
 
 namespace day02tests;
@@ -7,6 +8,26 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var s = new Submarine();
+        s.Forward(5);
+        s.Down(5);
+        s.Forward(8);
+        s.Up(3);
+        s.Down(8);
+        s.Forward(2);
+        s.MultipliedPosition.ShouldBe(150);
+    }
 
+    [Fact]
+    public void Test2()
+    {
+        var s = new Submarine();
+        s.ForwardWithAim(5);
+        s.AimDown(5);
+        s.ForwardWithAim(8);
+        s.AimUp(3);
+        s.AimDown(8);
+        s.ForwardWithAim(2);
+        s.MultipliedPosition.ShouldBe(900);
     }
 }
