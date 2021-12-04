dd316aacd704dce72b9382359644bbf7055ce70f
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-04 09:13:47 +0100

    Solution for day 4

--- day04/Program.cs
+++ day04/Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt")
+    .ToArray();
+
+int[] drawNumbers = lines[0].Split(',').Select(n => int.Parse(n)).ToArray();
+
+System.Console.WriteLine($"Numbers to draw: {drawNumbers.Length}");
+
+// Create games
+var games = new List<BingoGame>();
+for (int i = 2; i < lines.Length - 2; i += 6)
+{
+    var boardNumbers = To2D(lines[i..(i + 5)].Select(l => l.Split(" ", 5, StringSplitOptions.RemoveEmptyEntries).Select(n => int.Parse(n)).ToArray()).ToArray());
+    games.Add(new BingoGame(boardNumbers));
+}
+
+// Draw numbers and check for wins
+for (int i = 0; i < drawNumbers.Length; i++)
+{
+    foreach (var game in games)
+    {
+        if (game.FullRowOrColumn) continue;
+        if (game.Draw(drawNumbers[i]))
+        {
+            // Win
+            Console.WriteLine($"When {drawNumbers[i]} was drawn, board with index {games.IndexOf(game)} won with score {game.SumOfUnmarkedNumbers * drawNumbers[i]}");
+        }
+    }
+}
+
+int[,] To2D(int[][] source)
+{
+    var result = new int[5, 5];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            result[i, j] = source[i][j];
+        }
+    }
+    return result;
+}
+
+public class BingoGame
+{
+    int[,] _boardNumbers = new int[5, 5];
+    public BingoGame(int[,] boardNumbers)
+    {
+        _boardNumbers = boardNumbers;
+    }
+
+    public bool Draw(int number)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (_boardNumbers[i, j] == number)
+                {
+                    _boardNumbers[i, j] = -1;
+                }
+            }
+        }
+        return FullRowOrColumn;
+    }
+
+    public bool FullRowOrColumn
+    {
+        get
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (GetRow(_boardNumbers, i).Sum() == -5) return true;
+                if (GetColumn(_boardNumbers, i).Sum() == -5) return true;
+            }
+            return false;
+        }
+
+    }
+    public int SumOfUnmarkedNumbers
+    {
+        get
+        {
+            return _boardNumbers.Cast<int>().Where(n => n >= 0).Sum();
+        }
+    }
+    IEnumerable<int> GetRow(int[,] array, int row)
+    {
+        for (int i = 0; i <= array.GetUpperBound(1); ++i)
+            yield return array[row, i];
+    }
+
+    IEnumerable<int> GetColumn(int[,] array, int column)
+    {
+        for (int i = 0; i <= array.GetUpperBound(0); ++i)
+            yield return array[i, column];
+    }
+}
+
--- tests/day04tests/UnitTest1.cs
+++ tests/day04tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Shouldly;
 using Xunit;
 
 namespace day04tests;
@@ -7,6 +8,28 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        var boardNumbers = new int[,]
+        {
+            {14,21,17,24, 4},
+            {10,16,15, 9,19},
+            {18, 8,23,26,20},
+            {22,11,13, 6, 5},
+            { 2, 0,12, 3, 7},
+        };
+        var board = new BingoGame(boardNumbers);
+        board.Draw(7).ShouldBeFalse();
+        board.Draw(4).ShouldBeFalse();
+        board.Draw(9).ShouldBeFalse();
+        board.Draw(5).ShouldBeFalse();
+        board.Draw(11).ShouldBeFalse();
+        board.Draw(17).ShouldBeFalse();
+        board.Draw(23).ShouldBeFalse();
+        board.Draw(2).ShouldBeFalse();
+        board.Draw(0).ShouldBeFalse();
+        board.Draw(14).ShouldBeFalse();
+        board.Draw(21).ShouldBeFalse();
+        board.Draw(24).ShouldBeTrue();
 
+        board.SumOfUnmarkedNumbers.ShouldBe(188);
     }
 }
