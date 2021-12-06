e2eec894ede9b6e142b1fd2d4506ebfba5b62491
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-06 07:15:10 +0100

    Solution for part 1 of day 6

--- day06/Program.cs
+++ day06/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt")[0].Split(',').Select(n => int.Parse(n)).ToList();
+
+//var lines = new List<int> { 3, 4, 3, 1, 2 };
+
+for (int day = 0; day < 80; day++)
+{
+    var laternFisheCount = lines.Count;
+    for (int i = 0; i < laternFisheCount; i++)
+    {
+        if (lines[i] == 0)
+        {
+            lines[i] = 6;
+            lines.Add(8);
+        }
+        else
+        {
+            lines[i]--;
+        }
+    }
+}
+
+System.Console.WriteLine(lines.Count);
