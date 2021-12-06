04e7908cfaaa0468207e6c46128d909088430544
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-06 22:03:23 +0100

    Day06 (#8)
    * Add day06
    * Solution for part 1 of day 6
    * Needed a better solution for part 2

---
+++ day06/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllLines("input.txt")[0].Split(',').Select(n => int.Parse(n)).ToList();
+
+System.Console.WriteLine($"Part 1: {Simulate(input, 80)}");
+System.Console.WriteLine($"Part 2: {Simulate(input, 256)}");
+
+long Simulate(List<int> input, int days)
+{
+    long[] laternFishesAge = new long[9];
+    foreach (var item in input)
+    {
+        laternFishesAge[item]++;
+    }
+    for (int day = 0; day < days; day++)
+    {
+        long saveZeroes = laternFishesAge[0];
+
+        for (int age = 0; age < 8; age++)
+        {
+            laternFishesAge[age] = laternFishesAge[age + 1];
+        }
+
+        laternFishesAge[6] += saveZeroes;
+        laternFishesAge[8] = saveZeroes;
+    }
+    return laternFishesAge.Sum();
+}
