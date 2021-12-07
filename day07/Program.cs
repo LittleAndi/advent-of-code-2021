c66ae80f8a187fb7ab722f9febbf55b046d57531
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-07 21:06:10 +0100

    Day07 (#9)
    * Add day07
    * Switch to day 7
    * Solution for day 7

---
+++ day07/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllLines("input.txt")[0].Split(',').Select(n => int.Parse(n)).ToArray();
+
+var mean = input.OrderBy(n => n).ToArray()[input.Length / 2];
+System.Console.WriteLine($"Mean: {mean}");
+
+var fuel = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    fuel += Math.Abs(input[i] - mean);
+}
+
+System.Console.WriteLine($"Part 1: {fuel}");
+
+var avg = (int)Math.Round(input.Average(), 0) - 1;
+System.Console.WriteLine($"Avg: {avg}");
+
+fuel = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = 1; j <= Math.Abs(input[i] - avg); j++)
+    {
+        fuel += j;
+    }
+}
+
+System.Console.WriteLine($"Part 2: {fuel}");
