7b8b439f8758fcc4bd8bb82c9430d37ef22073e0
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-08 07:03:54 +0100

    Solution for part 1

--- day08/Program.cs
+++ day08/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt")
+    .Where(l => !string.IsNullOrWhiteSpace(l))
+    .Select(l => l.Split(" | "))
+    .ToArray();
+
+var output = lines.Select(l => l[1].Split(' '));
+
+int count = 0;
+foreach (var item in output)
+{
+    foreach (var j in item)
+    {
+        if (j.Length == 2 || j.Length == 3 || j.Length == 4 || j.Length == 7) count++;
+    }
+}
+
+System.Console.WriteLine(count);
+
+var displays = lines.Select(l => new Display(l[0].Split(' '), l[1].Split(' ')));
+
+public class Display
+{
+    string[] patterns;
+    string[] output;
+    public Display(string[] patterns, string[] output)
+    {
+        this.patterns = patterns;
+        this.output = output;
+    }
+
+
+}
