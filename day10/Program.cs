a0962123514c1fcfd9629b26c456297db186c0cf
Lars Nyström <nystrom.lasse@gmail.com>
2021-12-10 07:01:18 +0100

    We have a start...

--- day10/Program.cs
+++ day10/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var chunks = File.ReadAllLines("input_test.txt")
+    .Select(l => !string.IsNullOrWhiteSpace(l));
+
+public class NavigationSystem
+{
+    IEnumerable<string> chunks;
+    Dictionary<char, int> score = new Dictionary<char, int>
+    {
+        {')', 3 },
+        {']', 57 },
+        {'}', 1197 },
+        {'>', 25137 },
+    };
+
+    public NavigationSystem(IEnumerable<string> chunks)
+    {
+        this.chunks = chunks;
+    }
+    public int TotalSyntaxErrorScore
+    {
+        get
+        {
+            var totalSyntaxErrorScore = 0;
+            foreach (var chunk in chunks)
+            {
+                var firstIllegalCharacter = FirstIllegalCharacter(chunk);
+                if (firstIllegalCharacter != null)
+                {
+                    totalSyntaxErrorScore += score[(char)firstIllegalCharacter];
+                }
+            }
+            return totalSyntaxErrorScore;
+        }
+    }
+    char? FirstIllegalCharacter(string chunk)
+    {
+        Dictionary<char, int> closed = new Dictionary<char, int>()
+        {
+            {')', 0 },
+            {']', 0 },
+            {'}', 0 },
+            {'>', 0 },
+        };
+        foreach (var c in chunk.ToCharArray())
+        {
+            if (!closed.ContainsKey(c))
+            {
+                if (c == '(') closed[')']++;
+                if (c == '[') closed[']']++;
+                if (c == '{') closed['}']++;
+                if (c == '<') closed['>']++;
+            }
+            else
+            {
+                if (closed.Where(v => v.Key != c && v.Value > 0).Any()) return c;
+                if (--closed[c] < 0) return c;
+                closed[c]--;
+            }
+        }
+        return null;
+    }
+}
--- tests/day10tests/UnitTest1.cs
+++ tests/day10tests/UnitTest1.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Shouldly;
 using Xunit;
 
 namespace day10tests;
@@ -7,6 +11,20 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        var chunks = File.ReadAllLines("input_test.txt")
+            .Where(l => !string.IsNullOrWhiteSpace(l));
+        var nav = new NavigationSystem(chunks);
+        nav.TotalSyntaxErrorScore.ShouldBe(26397);
+    }
+    [Theory]
+    [InlineData("{([(<{}[<>[]}>{[]{[(<()>", 1197)]
+    [InlineData("[[<[([]))<([[{}[[()]]]", 3)]
+    [InlineData("[{[{({}]{}}([{[{{{}}([]", 57)]
+    [InlineData("{([(<{}[<>[]}>{[]{[(<()>", 3)]
+    [InlineData("<{([([[(<>()){}]>(<<{{", 25137)]
+    public void Test2(string chunk, int error)
+    {
+        var nav = new NavigationSystem(new List<string> { chunk });
+        nav.TotalSyntaxErrorScore.ShouldBe(error);
     }
 }
